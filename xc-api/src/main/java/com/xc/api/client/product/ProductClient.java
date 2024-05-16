a5156aef3532e76cd9b31c52c1dae69cb6fa1cf6
JirafaYe <2582101458@qq.com>
2024-05-16 20:23:22 +0800

    完成目录创建，以及根据id查询，暴露api接口

--- xc-api/src/main/java/com/xc/api/client/product/ProductClient.java
+++ xc-api/src/main/java/com/xc/api/client/product/ProductClient.java
@@ -2,7 +2,16 @@ package com.xc.api.client.product;
 
 import com.xc.api.client.product.fallback.ProductClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(value = "product-service", fallbackFactory = ProductClientFallback.class)
 public interface ProductClient {
+    /**
+     * 根据id查询category
+     * @param id 目录id
+     * @return
+     */
+    @GetMapping("/category/{id}")
+    String queryCategoryById(@PathVariable Long id);
 }
--- xc-product/src/main/java/com/xc/product/service/ICategoryService.java
+++ xc-product/src/main/java/com/xc/product/service/ICategoryService.java
@@ -2,6 +2,9 @@ package com.xc.product.service;
 
 import com.xc.product.entity.Category;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.xc.product.entity.vo.CategoryVO;
+
+import java.util.LinkedList;
 
 /**
  * <p>
@@ -12,5 +15,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2024-05-13
  */
 public interface ICategoryService extends IService<Category> {
+    boolean createCategory(CategoryVO vo);
 
+    LinkedList<String> queryCategoryById(Long id, LinkedList<String> result);
 }
