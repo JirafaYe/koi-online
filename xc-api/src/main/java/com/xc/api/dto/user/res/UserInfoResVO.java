d59646ee30717e743e6f890d45c2e37366e98998
pyl <3156358480@qq.com>
2024-05-16 13:08:26 +0800

    [修改]登录信息返回视图和实体类的关系 没啥

--- xc-api/src/main/java/com/xc/api/dto/user/res/UserInfoResVO.java
+++ xc-api/src/main/java/com/xc/api/dto/user/res/UserInfoResVO.java
@@ -6,36 +6,24 @@ import lombok.Data;
 
 public class UserInfoResVO {
 
-    private Long id;
-
+    private Long userId;
 
     private Integer userRole;
 
-
     private String mobile;
-
 
     private Integer status;
 
-
     private String account;
-
 
     private String nickName;
 
-
     private Integer gender;
-
 
     private String email;
 
-
     private String srcface;
-
-
 
     private Integer defaultAdress;
 
-
-    private String token;
 }
