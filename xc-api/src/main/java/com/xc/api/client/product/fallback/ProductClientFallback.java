a3613851ba0fe0f1ba1f1d19a21556a907c198c1
JirafaYe <2582101458@qq.com>
2024-05-16 20:34:51 +0800

    解决冲突

--- xc-api/src/main/java/com/xc/api/client/product/fallback/ProductClientFallback.java
+++ xc-api/src/main/java/com/xc/api/client/product/fallback/ProductClientFallback.java
@@ -1,8 +1,6 @@
 package com.xc.api.client.product.fallback;
 
 import com.xc.api.client.product.ProductClient;
-import com.xc.api.dto.user.LongIdsVO;
-import com.xc.api.dto.user.UserInfoResVO;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 
