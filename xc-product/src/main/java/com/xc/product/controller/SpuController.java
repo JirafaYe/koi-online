7f4b3bd60ed3ee2d36415a6525e75ce7389c16b7
JirafaYe <2582101458@qq.com>
2024-05-18 23:00:51 +0800

    完成分页查询

--- xc-product/src/main/java/com/xc/product/controller/SpuController.java
+++ xc-product/src/main/java/com/xc/product/controller/SpuController.java
@@ -1,11 +1,11 @@
 package com.xc.product.controller;
 
+import com.xc.common.domain.dto.PageDTO;
+import com.xc.product.entity.query.SpuQuery;
+import com.xc.product.entity.vo.SpuPageVO;
 import com.xc.product.entity.vo.SpuVO;
 import com.xc.product.service.IStandardProductUnitService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -37,5 +37,25 @@ public class SpuController {
     @PostMapping("/update")
     public boolean updateSpu(@RequestBody @Valid SpuVO vo){
         return spuService.updateSpu(vo);
+    }
+
+    /**
+     * 删除spu，同时删除所有sku
+     * @param id spuId
+     * @return
+     */
+    @PostMapping("/{id}")
+    public boolean remove(@PathVariable Long id){
+        return spuService.removeSpu(id);
+    }
+
+    /**
+     * 分页查询，可指定brand和category
+     * @param query
+     * @return
+     */
+    @GetMapping("/query")
+    public PageDTO<SpuPageVO> queryPage(SpuQuery query){
+        return spuService.queryByPage(query);
     }
 }
--- xc-product/src/main/java/com/xc/product/entity/vo/SpuPageVO.java
+++ xc-product/src/main/java/com/xc/product/entity/vo/SpuPageVO.java
@@ -1,4 +1,66 @@
 package com.xc.product.entity.vo;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
 public class SpuPageVO {
+    private Long id;
+
+    /**
+     * 商品名
+     */
+    private String spuName;
+
+    /**
+     * 品牌
+     */
+    private String  brand;
+
+    /**
+     * 分类
+     */
+    private String  category;
+
+    /**
+     * 图片list,以，分隔,最多8个
+     */
+    private String  mainImagesId;
+
+    /**
+     * 描述
+     */
+    private String content;
+
+    /**
+     * 详情图，id以，分隔,最多8个
+     */
+    private String  contentImagesId;
+
+    /**
+     * 视频id
+     */
+    private Long mainVideoId;
+
+    /**
+     * 默认为false
+     */
+    private Boolean available;
+
+    /**
+     * 库存
+     */
+    private Long num;
+
+    /**
+     * 创建人
+     */
+    private String creater;
+
+    /**
+     * 更新人
+     */
+    private String updater;
 }
